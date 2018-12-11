ff9d36fabcba2ca42125259a4215d46295a40efc
SamRichter01 <samrich9169@gmail.com>
2018-12-11 12:30:18 -0600

    Database uploading finished

---
+++ ISD196LunchroomApp/month.swift
@@ -0,0 +1,20 @@
+//
+//  Month.swift
+//  ISD196LunchroomApp
+//
+//  Created by Sam on 12/11/18.
+//  Copyright © 2018 district196.org. All rights reserved.
+//
+
+import Foundation
+
+class Month {
+    
+    var name: String
+    var days = [Day]()
+    
+    init (name: String) {
+        self.name = name
+    }
+    
+}
