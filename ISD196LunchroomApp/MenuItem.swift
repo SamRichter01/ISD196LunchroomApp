21eb847e91979c0f8f3c5de6a77ed60db62337dd
Josh <josh@ehs-s-xlib-109.local>
2018-12-12 10:36:54 -0600

    Created MenuItem class

---
+++ ISD196LunchroomApp/MenuItem.swift
@@ -0,0 +1,27 @@
+//
+//  MenuItem.swift
+//  ISD196LunchroomApp
+//
+//  Created by Josh on 12/11/18.
+//  Copyright © 2018 district196.org. All rights reserved.
+//
+
+import Foundation
+
+class MenuItem
+{
+    var itemType: String = "";
+    var name: String = "";
+    var price: Double = 0.0;
+    var description: String = "";
+    var ingredients: String = "";
+    
+    init(iT: String, n: String, p: Double, d: String, i: String)
+    {
+        itemType = iT
+        name = n
+        price = p
+        description = d
+        ingredients = i
+    }
+}
