a4e75a85c71e93c74214a6ffd23dba8f79749967
SamRichter01 <samrich9169@gmail.com>
2018-12-12 10:42:57 -0600

    Added master menu class

---
+++ ISD196LunchroomApp/MasterMenu.swift
@@ -0,0 +1,18 @@
+//
+//  MasterMenu.swift
+//  ISD196LunchroomApp
+//
+//  Created by Sam on 12/12/18.
+//  Copyright © 2018 district196.org. All rights reserved.
+//
+
+import Foundation
+
+var months = [Month]()
+var menuItems = [MenuItem]()
+var ALaCarteItems = [MenuItem]()
+
+class MasterMenu {
+    
+}
+
